e56714e98e1ee7745edd337581dc442d87859d12
Julien Fraichot <julien.fraichot@hyland.com>
2025-02-21 15:37:10 +0100

    feat(Seed): only log mnemonic when generated

--- src/keyGenerators/utils/generateSeed.js
+++ src/keyGenerators/utils/generateSeed.js
@@ -4,11 +4,11 @@ const log = require('../../utils/log');
 function generateSeed (mnemonic) {
   if (!mnemonic) {
     mnemonic = bip39.generateMnemonic()
+    log.spacer();
+    console.warn('mnenomic phrase generated (store it safely!):');
+    log.red(mnemonic);
+    log.spacer();
   }
-  log.spacer();
-  console.warn('mnenomic phrase generated (store it safely!):');
-  log.red(mnemonic);
-  log.spacer();
 
   return bip39.mnemonicToSeedSync(mnemonic);
 }
